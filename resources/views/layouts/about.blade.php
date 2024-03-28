6137f5a9d01d3d4a2cc865e6a08c3c4d69c4d409
Daniil Dybka <danil.dybko@gmail.com>
2024-03-29 02:27:01 +0300

    Update about.blade.php

--- resources/views/layouts/about.blade.php
+++ resources/views/layouts/about.blade.php
@@ -9,11 +9,13 @@
 @section('body')
 <body class="d-flex flex-column justify-content-between min-vh-100">
     @include('includes.about.header')
+
     <main class="flex-grow-1 pb-1">
         <section class="container">
             @yield('about')
         </section>
     </main>
+
     @include('includes.about.footer')
     @include('includes.about.cookie')
 
